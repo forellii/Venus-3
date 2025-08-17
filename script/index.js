c217f54186f4306796f3b395484d04073b51e36f
forellii <2008.mv.mv@gmail.com>
2025-08-17 19:07:29 -0300

    upd

--- script/index.js
+++ script/index.js
@@ -3,3 +3,18 @@
 //dc.addEventListener('click', () => {
 //    window.open('' , '_blank');
 //});/
+
+
+
+
+
+
+
+
+//mobile menu
+const menu = document.getElementById('menu');
+const btnOpen = document.getElementById('btn-open');
+
+btnOpen.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
