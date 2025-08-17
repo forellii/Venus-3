58c42e5339f8cf6995d40575593d5b06b7e66d72
forellii <2008.mv.mv@gmail.com>
2025-08-17 20:50:36 -0300

    upd

---
+++ script/galery.js
@@ -0,0 +1,34 @@
+const menu = document.getElementById('menu');
+const btnOpen = document.getElementById('btn-open');
+
+btnOpen.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
+
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+const btnNext = document.getElementById('dir');
+const btnPrev = document.getElementById('esq');
+
+function next() {
+    count++;
+    if (count > 12) {
+        count = 1;
+    }
+    document.getElementById("radio" + count).checked = true;
+}
+
+btnNext.addEventListener('click', () => {
+    next();
+});
+function prev() {
+    count--;
+    if (count < 1) {
+        count = 12;
+    }
+    document.getElementById("radio" + count).checked = true;
+}
+btnPrev.addEventListener('click', () => {
+    prev();
+});
