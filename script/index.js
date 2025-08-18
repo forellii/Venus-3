fc9a937abb10a30ef647db8347132fcae14460e9
forellii <2008.mv.mv@gmail.com>
2025-08-18 17:14:58 -0300

    upd

--- script/index.js
+++ script/index.js
@@ -18,3 +18,27 @@ const btnOpen = document.getElementById('btn-open');
 btnOpen.addEventListener('click', () => {
     menu.classList.toggle('open');
 });
+
+//Status do Servidor
+function atualizarStatus() {
+  fetch("https://api.mcstatus.io/v2/status/java/br-ultra-5.enxadahost.com:10006")
+    .then(res => res.json())
+    .then(data => {
+      let status = document.getElementById("status");
+      if (data.online) {
+        let players = data.players.online;
+        let max = data.players.max;
+        let version = data.version.name_raw;
+        status.innerHTML = `🟢 Online - ${players}/${max} jogadores`;
+      } else {
+        status.innerHTML = "Servidor Offline";
+      }
+    })
+    .catch(() => {
+      document.getElementById("status").innerHTML = "Erro ao buscar status";
+    });
+}
+
+atualizarStatus();
+
+setInterval(atualizarStatus, 10000);
