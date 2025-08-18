30e6c29da3dc04343f7ddf8d05305c13544e7ef4
forellii <2008.mv.mv@gmail.com>
2025-08-18 11:12:55 -0300

    upd

--- script/blog.js
+++ script/blog.js
@@ -95,3 +95,23 @@ const btnOpen = document.getElementById('btn-open');
 btnOpen.addEventListener('click', () => {
     menu.classList.toggle('open');
 });
+
+const pullToRefresh = document.querySelector('.pull-to-refresh');
+let touchstartY = 0;
+document.addEventListener('touchstart', e => {
+  touchstartY = e.touches[0].clientY;
+});
+document.addEventListener('touchmove', e => {
+  const touchY = e.touches[0].clientY;
+  const touchDiff = touchY - touchstartY;
+  if (touchDiff > 0 && window.scrollY === 0) {
+    pullToRefresh.classList.add('visible');
+    e.preventDefault();
+  }
+});
+document.addEventListener('touchend', e => {
+  if (pullToRefresh.classList.contains('visible')) {
+    pullToRefresh.classList.remove('visible');
+    location.reload();
+  }
+});
